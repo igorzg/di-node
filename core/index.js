bc0a105f17a07e89bdde87a25d9558ebd779e015
igorzg <igor.zg1987@gmail.com>
2015-09-09 20:32:50 +0200

    Added set file path and update of comments

--- core/index.js
+++ core/index.js
@@ -3,7 +3,7 @@
 let Type = require("typed-js");
 /**
  * @license Mit Licence 2014
- * @since 0.0.1
+ * @since 0.1.0
  * @author Igor Ivanovic
  * @name DI
  *
@@ -18,9 +18,8 @@ class DI extends Type {
             filePaths: Type.OBJECT
         });
     }
-
     /**
-     * @since 0.0.1
+     * @since 0.1.0
      * @author Igor Ivanovic
      * @function
      * @name DI#normalize
@@ -39,7 +38,7 @@ class DI extends Type {
         }
     }
     /**
-     * @since 0.0.1
+     * @since 0.1.0
      * @author Igor Ivanovic
      * @function
      * @name DI#hasAlias
@@ -51,16 +50,15 @@ class DI extends Type {
     hasAlias(key) {
         return this.aliases.hasOwnProperty(key);
     }
-
     /**
-     * @since 0.0.1
+     * @since 0.1.0
      * @author Igor Ivanovic
      * @function
      * @name DI#getAlias
      * @param {String} key
      *
      * @description
-     * Get an alias
+     * Get an path alias
      */
     getAlias(key) {
         if (this.hasAlias(key)) {
@@ -69,7 +67,7 @@ class DI extends Type {
         throw new Error(`DI.getAlias: ${key} is not in registered as alias`);
     }
     /**
-     * @since 0.0.1
+     * @since 0.1.0
      * @author Igor Ivanovic
      * @function
      * @name DI#setAlias
@@ -77,28 +75,83 @@ class DI extends Type {
      * @param {String} val
      *
      * @description
-     * Set an alias
+     * Set an path alias
      */
     setAlias(key, val) {
         this.aliases[key] = val;
     }
-
+    /**
+     * @since 0.1.0
+     * @author Igor Ivanovic
+     * @function
+     * @name DI#refereshNodeModuleCache
+     * @param {String} file
+     *
+     * @description
+     * Refreshes node module
+     */
     refereshNodeModuleCache(file) {
         let path = this.getFilePath(file);
         let key = require.resolve(path + ".js");
         delete require.cache[key];
         return path;
     }
-
+    /**
+     * @since 0.1.0
+     * @author Igor Ivanovic
+     * @function
+     * @name DI#hastFilePath
+     * @param {String} key
+     *
+     * @description
+     * Check if key exists in file paths
+     */
+    hastFilePath(key) {
+        return this.filePaths.hasOwnProperty(key);
+    }
+    /**
+     * @since 0.1.0
+     * @author Igor Ivanovic
+     * @function
+     * @name DI#hastFilePath
+     * @param {String} key
+     * @param {String} value
+     *
+     * @description
+     * Check if key exists in file paths
+     */
+    setFilePath(key, value) {
+        this.filePaths[key] = value;
+    }
+    /**
+     * @since 0.1.0
+     * @author Igor Ivanovic
+     * @function
+     * @name DI#getFilePath
+     * @param {String} key
+     *
+     * @description
+     * Return file path
+     */
     getFilePath(key) {
-        if (this.filePaths.hasOwnProperty(key)) {
+        if (this.hastFilePath(key)) {
             key = this.filePaths[key];
         }
         return this.normalize(key);
     }
-
+    /**
+     * @since 0.1.0
+     * @author Igor Ivanovic
+     * @function
+     * @name DI#mock
+     * @param {String} file
+     * @param {Object} mocks
+     *
+     * @description
+     * Return module with mocked objects
+     */
     mock(file, mocks) {
-        let load =  DI.prototype.load;
+        let load = DI.prototype.load;
         // mock load
         DI.prototype.load = name => {
             return mocks[name];
@@ -118,7 +171,16 @@ class DI extends Type {
             DI.prototype.load = load;
         }
     }
-
+    /**
+     * @since 0.1.0
+     * @author Igor Ivanovic
+     * @function
+     * @name DI#load
+     * @param {String} key
+     *
+     * @description
+     * Return loaded module
+     */
     load(key) {
         try {
             return require(this.getFilePath(key));
